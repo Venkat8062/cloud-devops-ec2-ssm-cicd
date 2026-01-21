dc4f2146f8c0466f5634064fe6aec69ac9496603
Venkatraman S <venkatraman8062@gmail.com>
2026-01-21 12:56:59 +0530

    Add new variables for availability zone, AMI ID, and instance type

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,19 @@ variable "aws_region" {
   description = "AWS region to deploy resources into"
   type        = string
 }
+
+variable "availability_zone" {
+  description = "Availability zone for the public subnet (e.g. us-east-1a)"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance (region-specific)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
